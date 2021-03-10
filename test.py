2fb2694eeec07394e0b776dda43853b8a6f2e826
Dustin Aldana <dustin.j.aldana@gmail.com>
2021-03-10 09:08:38 -0800

    test

--- test.py
+++ test.py
@@ -2,8 +2,29 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "tables/nodes")
+print("####Reference Test#####")
+response = requests.get(BASE + "/tables/reference-list/1")
+print(response.json())
 
+print("\n####Print Tables Test#####")
+response = requests.get(BASE + "/tables")
+print(response.json())
+
+print("\n####Print Columns From Table#####")
+response = requests.get(BASE + "/tables/nodes/attributes")
+print(response.json())
+
+print("\n####Print Rows From Specific Table (this example only prints one row)#####")
+response = requests.get(BASE + "/tables/nodes")
 for row in response.json():
     print(row)
     break
+
+print("\n####Specify the Province####")
+response = requests.get(BASE + "/tables/nodes/NL")
+for row in response.json():
+    print(row)
+    break
+#for row in response.json():
+ #   print(row)
+  #  break
