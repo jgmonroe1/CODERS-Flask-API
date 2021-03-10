394c6f6a375f31b1d32d012056118d1b830d7915
Dustin Aldana <dustin.j.aldana@gmail.com>
2021-03-10 14:26:06 -0800

    removing ability to ask for node table

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 
 ##Configure db
-db = yaml.load(open('db.yaml'))
+db = yaml.load(open('db.yaml'), Loader=yaml.FullLoader)
 app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
@@ -15,6 +15,7 @@ app.config['MYSQL_DB'] = db['mysql_db']
 
 mysql = MySQL(app)
 
+##Executes the query to the database
 def send_query(query):
     cur = mysql.connection.cursor()
     try:
@@ -24,38 +25,121 @@ def send_query(query):
     except:
         return 0
 
+##Returns the columns of a specified table
 def get_columns(table):
     columns = []
-    query = f"SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{table}' ORDER BY ORDINAL_POSITION"
-    results = send_query(query)
-    for column in results:
-        columns.append(column[0])
+    if table == 'substations':
+        columns = ["name", 
+                        "node_code", 
+                        "node_type",
+                        "substation_type", 
+                        "owner", 
+                        "province", 
+                        "latitude", 
+                        "longitude", 
+                        "planning_region", 
+                        "sources", 
+                        "notes"]
+    elif table == 'interties':
+        columns = ["name", 
+                        "node_code", 
+                        "node_type",
+                        "intertie_type", 
+                        "owner", 
+                        "province", 
+                        "latitude", 
+                        "longitude", 
+                        "planning_region", 
+                        "sources", 
+                        "notes"]
+    else:   
+        query = f"SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{table}' ORDER BY ORDINAL_POSITION"
+        results = send_query(query)
+        for column in results:
+            columns.append(column[0])
     return columns
-
 
 ##Returns a list of the tables in the DB
 @app.route('/tables', methods=['GET'])
 def show_db_tables():
-    query = "SHOW tables;"
-    results = send_query(query)
-    tables = jsonify(results)
-    return tables
+    tables = ["generators",
+                "substations",
+                "transmission_lines",
+                "storage_batteries",
+                "interties",
+                "distribution_transfers",
+                "contribution_transfers",
+                "generation_costs",
+                "international_transfers",
+                "interprovincial_transfers",
+                "provincial_transfers",
+                "intertie_all",
+                "intertie_provincial",
+                "cpi_can",
+                "reference_list"]
+
+    return jsonify(tables)
 
 ##Returns the full table 
 @app.route('/tables/<string:table>', methods=['GET'])
 def return_table(table):
-    ##get the columns from the specified table
+    ##if the table is sub, jct, or int, join the subtable on the node table
+    if table == "junctions":
+        table = "nodes"
+        query = f"SELECT * FROM {table} WHERE node_type = 'JCT';"
+        if column_names == 0:
+            return jsonify("Table does not exist.")
+
+    ##Join the substation on the nodes
+    elif table == "substations":
+        query = f"SELECT \
+                    n.name, \
+                    n.node_code, \
+                    n.node_type, \
+                    s.sub_type, \
+                    n.owner, \
+                    n.province, \
+                    n.latitude, \
+                    n.longitude, \
+                    n.planning_region, \
+                    n.sources, \
+                    n.notes \
+                    FROM nodes n \
+                    JOIN substations s ON n.node_code = s.sub_node_code;"
+        
+    ##Join the interties on the nodes
+    elif table == "interties":
+        query = f"SELECT \
+                    n.name, \
+                    n.node_code, \
+                    n.node_type, \
+                    i.intertie_type, \
+                    n.owner, \
+                    n.province, \
+                    n.latitude, \
+                    n.longitude, \
+                    n.planning_region, \
+                    n.sources, \
+                    n.notes \
+                    FROM nodes n \
+                    JOIN interties i ON n.node_code = i.int_node_code;"
+
+    ##If not a jct, sub, or int, query the table
+    else:
+        query = f"SELECT * FROM {table};"
+        ##get the columns from the specified table
+        column_names = get_columns(table)
+        if column_names == 0:
+            return jsonify("Table does not exist.")
+    
     column_names = get_columns(table)
-    if column_names == 0:
-        return "Table does not exist."
     
     ##send the query
-    query = f"SELECT * FROM {table};"
     result = send_query(query)
 
     ##the table name is incorrect
     if result == 0:
-        return  jsonify("Table does not exist")
+        return  jsonify("Table is empty.")
     result = list(result)
     ##formats the list to "'column_name': 'value'"
     for row_idx,row in enumerate(result):
@@ -63,13 +147,20 @@ def return_table(table):
         for i,column in enumerate(row):
             row[i] = {column_names[i]: column}    
         result[row_idx] = row
+
+    ##Check if the table is empty
+    if len(result) == 0:
+        return jsonify("Table is empty.")
+        
     return jsonify(result)
 
 ##Returns the columns from a specified table
 @app.route('/tables/<string:table>/attributes', methods=['GET'])
 def return_columns(table):
-    query = f"SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{table}' ORDER BY ORDINAL_POSITION"
-    attributes = send_query(query)
+    if table == "junctions":
+        table = "nodes"
+    
+    attributes = get_columns(table)
 
     ##check if table name is incorrect
     if attributes == 0:
@@ -106,8 +197,46 @@ def return_ref(key):
 ##Returns the specified table based on Province
 @app.route('/tables/<string:table>/<string:province>', methods=['GET'])
 def return_based_on_prov(table, province):
-    ##query the table based on province
-    query = f"SELECT * FROM {table} WHERE province = '{province}';"
+    ##query for substations joined on nodes
+    if table == "interties":
+        query = f"SELECT \
+                    n.name, \
+                    n.node_code, \
+                    n.node_type, \
+                    i.intertie_type, \
+                    n.owner, \
+                    n.province, \
+                    n.latitude, \
+                    n.longitude, \
+                    n.planning_region, \
+                    n.sources, \
+                    n.notes \
+                    FROM nodes n \
+                    JOIN interties i ON n.node_code = i.int_node_code \
+                    WHERE n.province = '{province}';"
+    ##query for junctions joined on nodes
+    elif table == "substations":
+        query = f"SELECT \
+                    n.name, \
+                    n.node_code, \
+                    n.node_type, \
+                    s.sub_type, \
+                    n.owner, \
+                    n.province, \
+                    n.latitude, \
+                    n.longitude, \
+                    n.planning_region, \
+                    n.sources, \
+                    n.notes \
+                    FROM nodes n \
+                    JOIN substations s ON n.node_code = s.sub_node_code \
+                    WHERE n.province = '{province}';"
+    ##query only the junction types from the node table
+    elif table == "junctions":
+        table = "nodes"
+        query = f"SELECT * FROM {table} WHERE province = '{province}' \
+                AND node_type = 'JCT';"
+    
     result = send_query(query)
 
     ##get the column names
@@ -126,7 +255,13 @@ def return_based_on_prov(table, province):
         for i,column in enumerate(row):
             row[i] = {column_names[i]: column}    
         result[row_idx] = row
+    
+    ##Check if the table is empty
+    if len(result) == 0:
+        return jsonify("Table is empty.")
+
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    #app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True)
--- test.py
+++ test.py
@@ -2,29 +2,26 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-print("####Reference Test#####")
-response = requests.get(BASE + "/tables/reference-list/1")
-print(response.json())
+# print("####Reference Test#####")
+# response = requests.get(BASE + "/tables/reference-list/1")
+# print(response.json())
 
-print("\n####Print Tables Test#####")
-response = requests.get(BASE + "/tables")
-print(response.json())
+# print("\n####Print Tables Test#####")
+# response = requests.get(BASE + "/tables")
+# print(response.json())
 
-print("\n####Print Columns From Table#####")
-response = requests.get(BASE + "/tables/nodes/attributes")
-print(response.json())
+# print("\n####Print Columns From Table#####")
+# response = requests.get(BASE + "/tables/cpi_can/attributes")
+# print(response.json())
 
-print("\n####Print Rows From Specific Table (this example only prints one row)#####")
-response = requests.get(BASE + "/tables/nodes")
-for row in response.json():
-    print(row)
-    break
+# print("\n####Print Rows From Specific Table (this example only prints one row)#####")
+# response = requests.get(BASE + "/tables/substations")
+# for row in response.json():
+#     print(row)
+#     break
 
 print("\n####Specify the Province####")
-response = requests.get(BASE + "/tables/nodes/NL")
+response = requests.get(BASE + "/tables/junctions/NL")
 for row in response.json():
     print(row)
     break
-#for row in response.json():
- #   print(row)
-  #  break
