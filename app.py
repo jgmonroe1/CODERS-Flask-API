bb600d777f03eccc1c7dd7a2a80eabf9ce8e2704
Tristan Cusi <cusitristan@gmail.com>
2021-03-10 10:02:47 -0800

    added what port to use

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from flask_mysqldb import MySQL
 import sys
 import yaml
+import os
 
 app = Flask(__name__)
 
@@ -64,4 +65,4 @@ def return_table(table):
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
