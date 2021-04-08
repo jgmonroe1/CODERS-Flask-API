c32b089d6622f57c52141a9a3fd9aff9a935ccf4
Dustin Aldana <dustin.j.aldana@gmail.com>
2021-04-08 15:50:22 -0700

    added tests for province and transfers

--- integration_test.py
+++ integration_test.py
@@ -1,6 +1,7 @@
 import unittest
 import requests
 import mysql.connector
+import decimal 
 
 BASE = "http://127.0.0.1:5000/"
 
@@ -10,7 +11,7 @@ class Tests(unittest.TestCase):
         host='localhost', 
         user='root', 
         password='', 
-        database='CODERS_draft'
+        database='coders_draft'
     )
     cursor = db.cursor()
 
@@ -30,7 +31,7 @@ class Tests(unittest.TestCase):
                         "intertie_all",
                         "intertie_provincial",
                         "cpi_can",
-                        "references")
+                        "reference_list")
 
     def send_query(self, query):
         cur = self.db.cursor()
@@ -38,7 +39,7 @@ class Tests(unittest.TestCase):
             cur.execute(query)
             result = cur.fetchall()
             return list(result)
-        except E:
+        except:
             print("issue with sql query")
 
     def test_return_table(self):
@@ -179,7 +180,7 @@ class Tests(unittest.TestCase):
 
     def test_return_ref_w_good_id(self):
         #Arrange
-        ref_id = 9100
+        ref_id = 1
         query = f"SELECT * FROM reference_list WHERE id = {ref_id}"
         db_result = self.send_query(query)
 
@@ -192,9 +193,150 @@ class Tests(unittest.TestCase):
         #Assert
         self.assertEqual(response_code, 200)
         self.assertEqual(type(response_list), list)
+        self.assertEqual(len(response_list), len(db_result))
         for i,column in enumerate(response_row):
             self.assertEqual(column, db_result[0][i])
 
+    def test_return_based_on_province(self):
+        #Arrange
+        province = 'AB'
+        for table in self.accessible_tables:
+            if table in  ("distribution_transfers",
+                            "contribution_transfers",
+                            "generation_costs",
+                            "international_transfers",
+                            "interprovincial_transfers",
+                            "provincial_demand",
+                            "intertie_all",
+                            "intertie_provincial",
+                            "cpi_can",
+                            "reference_list"):
+                continue
 
+            if table == "interties":
+                query = f"SELECT \
+                            n.name, \
+                            n.node_code, \
+                            n.node_type, \
+                            i.intertie_type, \
+                            n.owner, \
+                            n.province, \
+                            n.latitude, \
+                            n.longitude, \
+                            n.planning_region, \
+                            n.sources, \
+                            n.notes \
+                            FROM nodes n \
+                            JOIN interties i ON n.node_code = i.int_node_code \
+                            WHERE n.province = '{province}';"
+            ## Query for substations joined on nodes
+            elif table == "substations":
+                query = f"SELECT \
+                            n.name, \
+                            n.node_code, \
+                            n.node_type, \
+                            s.sub_type, \
+                            n.owner, \
+                            n.province, \
+                            n.latitude, \
+                            n.longitude, \
+                            n.planning_region, \
+                            n.sources, \
+                            n.notes \
+                            FROM nodes n \
+                            JOIN substations s ON n.node_code = s.sub_node_code \
+                            WHERE n.province = '{province}';"
+            elif table == "junctions":
+                query = f"SELECT * FROM nodes WHERE node_type = 'JCT' AND province = '{province}';"
+            else:
+                query = f"SELECT * FROM {table} WHERE province = '{province}';"       
+
+            #ACT 
+            db_result = self.send_query(query)
+            response = requests.get(BASE + f"{table}?province={province}")
+            response_code = response.status_code
+            response_list = response.json()
+            ##AB contains no interties
+            if table == 'interties': continue
+            response_first_row = response_list[0]
+            response_first_row = response_first_row.values()
+            response_last_row = response_list[-1].values()
+            
+            #Assert
+            self.assertEqual(response_code, 200)
+            self.assertEqual(type(response_list), list)
+            for i,column in enumerate(response_first_row):
+                if type(db_result[0][i]) == decimal.Decimal:
+                    db_result[0] = list(db_result[0])
+                    db_result[0][i] = float(db_result[0][i])
+                self.assertEqual(column, db_result[0][i])
+
+            for i,column in enumerate(response_last_row):
+                if type(db_result[-1][i]) == decimal.Decimal:
+                    db_result[-1] = list(db_result[-1])
+                    db_result[-1][i] = float(db_result[-1][i])
+                self.assertEqual(column, db_result[-1][i])
+
+    def return_international_hourly_transfers(self):
+        ##Arrange
+        province = 'AB'
+        us_region = 'US-Montana'
+        year = 2018
+        query = f"SELECT * FROM international_transfers WHERE local_time LIKE '2018%'AND province = '{province}' and us_state = '{us_region}';"
+
+        #Act
+        db_result = self.send_query(query)
+        response = requests.get(BASE + f"international_transfers?year={year}&province={province}&us_region={US-Montana}")
+        response_code = response.status_code
+        response_list = response.json()
+        response_reow = response_list[0].values()
+
+        #Assert
+        self.assertEqual(response_code, 200)
+        self.assertEqual(type(response_list),list)
+        self.assertEqual(len(response_list), len(db_result))
+        for i,column in enumerate(response_row):
+            self.assertEqual(column,db_result[0][i])
+
+    def return_interprovincial_hourly_transfers(self):
+        ##Arrange
+        province1 = 'BC'
+        province2 = 'AB-AESO'
+        year = 2018
+        query = f"SELECT * FROM international_transfers WHERE local_time LIKE '{year}%'AND province_1 = '{province1}' and province_2 = '{province2}';"
+
+        #Act
+        db_result = self.send_query(query)
+        response = requests.get(BASE + f"interprovincial_transfers?year={year}&province1={province1}&province2={province2}")
+        response_code = response.status_code
+        response_list = response.json()
+        response_reow = response_list[0].values()
+
+        #Assert
+        self.assertEqual(response_code, 200)
+        self.assertEqual(type(response_list),list)
+        self.assertEqual(len(response_list), len(db_result))
+        for i,column in enumerate(response_row):
+            self.assertEqual(column,db_result[0][i])
+
+    def return_provincial_hourly_demand(self):
+        ##Arrange
+        province = 'AB'
+        year = 2018
+        query = f"SELECT * FROM international_transfers WHERE local_time LIKE '{year}%' AND province_1 = '{province1}';"
+
+        #Act
+        db_result = self.send_query(query)
+        response = requests.get(BASE + f"interprovincial_transfers?year={year}&province1={province1}&province2={province2}")
+        response_code = response.status_code
+        response_list = response.json()
+        response_reow = response_list[0].values()
+
+        #Assert
+        self.assertEqual(response_code, 200)
+        self.assertEqual(type(response_list),list)
+        self.assertEqual(len(response_list), len(db_result))
+        for i,column in enumerate(response_row):
+            self.assertEqual(column,db_result[0][i])
 if __name__ == '__main__':
 	unittest.main()
